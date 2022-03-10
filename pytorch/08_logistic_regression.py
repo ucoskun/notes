8ca29a0e44fe235e6fb8febfe7ac4bfe0efdc54a
Umit Coskun <uhcoskun@outlook.com>
2022-03-10 13:21:02 -0800

    New scripts added, wine data set

--- pytorch/08_logistic_regression.py
+++ pytorch/08_logistic_regression.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import numpy as np
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import learning_curve, train_test_split
 
 # data prep
 bc = datasets.load_breast_cancer()
@@ -13,7 +13,55 @@ n_samples, n_features = X.shape
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-# feature scaling
+# feature scaling 
+# https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+X_train = torch.from_numpy(X_train.astype(np.float32))
+X_test = torch.from_numpy(X_test.astype(np.float32))
+y_train = torch.from_numpy(y_train.astype(np.float32)).view(-1,1)
+y_test = torch.from_numpy(y_test.astype(np.float32)).view(-1,1)
+
+class LogisticRegression(nn.Module):
+    def __init__(self, n_input_features):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(n_input_features, 1)
+
+    def forward(self, x):
+        y_predicted = torch.sigmoid(self.linear(x))
+        return y_predicted
+
+model = LogisticRegression(n_features)
+
+# Loss and optimizer
+learning_rate = 0.01
+criterion = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# Training
+num_epochs = 1000
+
+for epoch in range(num_epochs):
+    # Forward pass and loss calculation
+    y_predicted = model(X_train)
+    loss = criterion(y_predicted, y_train)
+
+    # Backward pass
+    loss.backward()
+
+    # Updates
+    optimizer.step()
+
+    # Zero gradients
+    optimizer.zero_grad()
+
+    if (epoch+1) % 10 ==0:
+        print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
+
+with torch.no_grad():
+    y_predicted = model(X_test)
+    y_predicted_cls = y_predicted.round()
+    accuracy = y_predicted_cls.eq(y_test).sum() / float(y_test.shape[0])
+    print(f'accuracy = {accuracy:.4f}')
