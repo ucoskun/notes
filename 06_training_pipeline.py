b0087c1f79cd2865ba908c856cea6ed9110a2e3a
Umit Coskun <ucoskun@outlook.com>
2022-03-09 21:25:25 -0800

    Initial commit

---
+++ 06_training_pipeline.py
@@ -0,0 +1,7 @@
+# 1) Design model (input, output size, forward pass)
+# 2) Construct loops and optimizer
+# 3) Training loop
+#   - forward pass: compute prediction
+#   - backward pass: calculate gradients
+
+import torch
